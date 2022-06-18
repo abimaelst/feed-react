76735b9bb10c9c25c139c4740aaa95ac508a85f7
abimael silva tavares <abimael@MacBook-Pro.local>
2022-06-18 14:19:59 -0300

    config project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import { Header } from "../src/components/Header";
+import "./global.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
